93aaeedc36f5eba38eef574f38eee4642563f21a
Jonathan <jonathan.diaz714@gmail.com>
2022-05-22 21:11:16 -0400

    styling users index page

--- frontend/components/user/user_index_component.jsx
+++ frontend/components/user/user_index_component.jsx
@@ -12,7 +12,13 @@ class UserIndex extends React.Component {
 
     render() {
         const users = this.props.users.map(user => {
-            return  <div className='user'>
+            let facebook_url = null;
+            if (user.facebook_url) {
+                facebook_url = <a href={"https://" + user.facebook_url} target="_blank" rel="noopener noreferrer">
+                    <img src={window.facebookIcon} className="social-icon" alt="" />
+                </a>
+            }
+            return  <div className='user' key={user.id}>
                         <div>
                             {user.first_name}
                         </div>
@@ -21,6 +27,12 @@ class UserIndex extends React.Component {
                         </div>
                         <div>
                             {user.personal_email}
+                        </div>
+                        <div>
+                            {facebook_url}
+                            <a href={"https://" + user.linkedin_url} target="_blank" rel="noopener noreferrer">
+                                <img src={window.linkedInIcon} className="social-icon" alt="" />
+                            </a>
                         </div>
                     </div>
         })
